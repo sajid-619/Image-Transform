f85689a085ae6e6d6be6370bfb196f3ec3a93518
Sajid Zaman <zsajid014@gmail.com>
2019-12-15 14:21:55 +0000

    YML packages

--- uiuc/HSLAPixel.h
+++ uiuc/HSLAPixel.h
@@ -15,19 +15,26 @@ namespace uiuc {
   // Put your HSLAPixel class definition here.
   // (Remember to end it with a semicolon!)
   class HSLAPixel {
-      public:
-      double h, s, l, a;
-      HSLAPixel();
-      HSLAPixel(double hue, double saturation, double luminance);
-      HSLAPixel(double hue, double saturation, double luminance, double alpha);
+    public:
+    double h; 
+    double s; 
+    double l; 
+    double a; 
+    HSLAPixel();
 
+    HSLAPixel(double hue, double saturation, double luminance);
 
+    HSLAPixel(double hue, double saturation, double luminance, double alpha);
 
-
-      bool operator== (HSLAPixel const & other) const ;
-      bool operator!= (HSLAPixel const & other) const ;
-      std::ostream & operator<<(std::ostream & out, HSLAPixel const & pixel);
+    HSLAPixel & operator=(HSLAPixel const & other);
+    bool operator== (HSLAPixel const & other) const ;
+    bool operator!= (HSLAPixel const & other) const ;
+    bool operator<  (HSLAPixel const & other) const ;
   };
 
-
+  std::ostream & operator<<(std::ostream & out, HSLAPixel const & pixel);
+  std::stringstream & operator<<(std::stringstream & out, HSLAPixel const & pixel);
 }
+
+
+
